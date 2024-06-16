525aa91e2f1aa473e99572024bc7455a7815056c
Pavan Kumar <gopidesupavan@gmail.com>
2024-06-16 09:29:45 +0100

    doc clsf

--- tests/providers/amazon/aws/waiters/test_comprehend.py
+++ tests/providers/amazon/aws/waiters/test_comprehend.py
@@ -31,6 +31,7 @@ from airflow.providers.amazon.aws.sensors.comprehend import (
 class TestComprehendCustomWaiters:
     def test_service_waiters(self):
         assert "pii_entities_detection_job_complete" in ComprehendHook().list_waiters()
+        assert "create_document_classifier_complete" in ComprehendHook().list_waiters()
 
 
 class TestComprehendCustomWaitersBase:
@@ -64,6 +65,37 @@ class TestComprehendStartPiiEntitiesDetectionJobCompleteWaiter(TestComprehendCus
     def test_pii_entities_detection_job_wait(self, mock_get_job):
         wait = {"PiiEntitiesDetectionJobProperties": {"JobStatus": "IN_PROGRESS"}}
         success = {"PiiEntitiesDetectionJobProperties": {"JobStatus": "COMPLETED"}}
+        mock_get_job.side_effect = [wait, wait, success]
+
+        ComprehendHook().get_waiter(self.WAITER_NAME).wait(
+            JobId="job_id", WaiterConfig={"Delay": 0.01, "MaxAttempts": 3}
+        )
+
+
+class TestComprehendDocumentClassifierCompleteWaiter(TestComprehendCustomWaitersBase):
+    WAITER_NAME = "create_document_classifier_complete"
+
+    @pytest.fixture
+    def mock_get_job(self):
+        with mock.patch.object(self.client, "describe_document_classifier") as mock_getter:
+            yield mock_getter
+
+    @pytest.mark.parametrize("state", ComprehendCreateDocumentClassifierCompletedSensor.SUCCESS_STATES)
+    def test_pii_entities_detection_job_complete(self, state, mock_get_job):
+        mock_get_job.return_value = {"DocumentClassifierProperties": {"Status": state}}
+
+        ComprehendHook().get_waiter(self.WAITER_NAME).wait(DocumentClassifierArn="arn")
+
+    @pytest.mark.parametrize("state", ComprehendCreateDocumentClassifierCompletedSensor.FAILURE_STATES)
+    def test_pii_entities_detection_job_failed(self, state, mock_get_job):
+        mock_get_job.return_value = {"DocumentClassifierProperties": {"Status": state}}
+
+        with pytest.raises(botocore.exceptions.WaiterError):
+            ComprehendHook().get_waiter(self.WAITER_NAME).wait(DocumentClassifierArn="arn")
+
+    def test_pii_entities_detection_job_wait(self, mock_get_job):
+        wait = {"DocumentClassifierProperties": {"Status": "TRAINING"}}
+        success = {"DocumentClassifierProperties": {"Status": "TRAINED"}}
         mock_get_job.side_effect = [wait, wait, success]
 
         ComprehendHook().get_waiter(self.WAITER_NAME).wait(
